b38395bc0aa4c59408e9a2d03e9e921dac03d2cc
viaro-edgar <edgar.vega@viaro.net>
2018-11-27 16:04:00 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(76.52f);
+            book.AddGrade(89.6f);
+            book.AddGrade(92.1f);
+            book.AddGrade(73.8f);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.HighestGrade);
+        }
+    }
+}
